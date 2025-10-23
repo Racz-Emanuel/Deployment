cc2630c8b12015e31d2c5cbce703fc2460c6d7be
Radu Crisan <radu.crisan@ekkode.com>
2025-10-23 11:52:41 +0300

    add vue stuff

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
-<script setup></script>
+<script setup>
+import { onMounted, onUpdated } from 'vue'
+
+onMounted(() => {
+  console.log('App mounted')
+})
+
+onUpdated(() => {
+  console.log('App updated')
+})
+
+import { ref } from 'vue'
+const message = ref("Hello from Vue!")
+
+const clickHandler = () => {
+  message.value = "Button clicked!"
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>You did it!</h1> 
+  <button @click="clickHandler">Click me</button>
+  <p>{{ message }}</p>
+  <br>
+  <input type="text" 
+     @change="console.log('Text changed')" 
+     @input="console.log('You have typed: ' + $event.target.value)"
+     @keydown="console.log('Key pressed: ' + $event.key)"></input>
 </template>
 
 <style scoped></style>
