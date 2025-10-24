ee5ee13872ce8a989c9c47a1185db0b3fd287971
Radu Crisan <radu.crisan@ekkode.com>
2025-10-24 13:25:45 +0300

    feat: add Tailwind CSS and update App.vue with new features
    - Added Tailwind CSS and its Vite plugin as dependencies in package.json.
    - Updated App.vue to include:
      - A link to Emanuel's website.
      - A button to generate a random number with conditional display.
      - A counter with two increment methods.
    - Modified the style section to import Tailwind CSS.
    - Updated vite.config.js to include the Tailwind CSS plugin.

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,21 @@ onUpdated(() => {
 
 import { ref } from 'vue'
 const message = ref("Hello from Vue!")
+const link = ref("https://emanuel.ro")
+const target = ref("_blank")
 
 const clickHandler = () => {
   message.value = "Button clicked!"
+}
+
+const randomNumber = ref(null)
+const updateRandomNumber = () => {
+  randomNumber.value = Math.random()
+}
+
+const counter = ref(0)
+const incrementCounter = () => {
+  counter.value++
 }
 </script>
 
@@ -23,9 +35,31 @@ const clickHandler = () => {
   <p>{{ message }}</p>
   <br>
   <input type="text" 
+     v-model="message"
      @change="console.log('Text changed')" 
      @input="console.log('You have typed: ' + $event.target.value)"
      @keydown="console.log('Key pressed: ' + $event.key)"></input>
+  <br>
+  <a :href="link" :target="target">Visit Emanuel website</a>
+  <br>
+  <hr>
+  <h3>Display a random number</h3>
+  <button @click="updateRandomNumber">Get a random number {{ randomNumber }}</button>
+  <div v-if="randomNumber">
+    <div v-if="randomNumber >= 0.5">Number is bigger or equal than 0.5</div>
+    <div v-else>Number is lower than 0.5</div>
+  </div>
+  <br>
+  <div v-if="randomNumber && randomNumber >= 0.5">Number is bigger or equal than 0.5</div>
+  <div v-else-if="randomNumber">Number is lower than 0.5</div>
+  <br>
+  <hr>
+  <h3>Counter</h3>
+  <button @click="counter++">Increment v1</button>
+  <button @click="incrementCounter">Increment v2</button>
+  <div>{{ counter }}</div>
 </template>
 
-<style scoped></style>
+<style>
+ @import "tailwindcss";
+</style>
