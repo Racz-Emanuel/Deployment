fbad15f180da5178df826fd44ceef11787d80f38
Radu Crisan <radu.crisan@ekkode.com>
2025-12-04 11:50:15 +0200

    feat: add TaskPage component, integrate counter store, and update routing

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,11 @@
 
 <template>
  <header>
-     <nav>
+     <nav class="flex gap-4 mb-4">
        <RouterLink to="/">Home</RouterLink>
        <RouterLink to="/flowers">Flowers</RouterLink>
        <RouterLink to="/computed">Computed</RouterLink>
+       <RouterLink to="/tasks">Tasks</RouterLink>
      </nav>
  </header>
 
---
+++ src/pages/TaskPage.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useCounter } from '@/stores/counter';
+const counter = useCounter();
+
+</script>
+
+<template>
+    <h3>Task Page</h3>
+    Store counter demonstration!
+    <button type="button" class="border" @click="counter.count++"> 
+        Count Your Blessings {{ counter.count }}
+    </button>
+
+</template>
